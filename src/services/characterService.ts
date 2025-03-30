2a687ac690b07875b90c15c404e2df83fd661e9f
amobasse <amobasse@gmail.com>
2025-03-30 03:32:51 -0400

    added create and get all character functions to character service

---
+++ src/services/characterService.ts
@@ -0,0 +1,45 @@
+import CharacterCard from '../types/CharacterCard';
+
+const API_URL = 'http://localhost:3001/api';
+
+export const getUserCharacters = async (userId: string): Promise<CharacterCard[]> => {
+    try {
+        const response = await fetch(`${API_URL}/characters/${userId}`);
+        const data = response.json();
+
+        if (!response.ok) {
+            console.error(`Fetching the characters failed: ${data.message}`);
+            return [];
+        }
+
+        return data.characters;
+    } catch (error) {
+        console.error(`Error fetching characters: ${error}`);
+        return [];
+    }
+};
+
+export const createCharacter = async (character: Omit<CharacterCard, 'id'>) => {
+    try {
+        const response = await fetch(`${API_URL}/characters`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(character),
+            
+        });
+
+        const data = response.json();
+
+        if (!response.ok) {
+            console.error(`Something went wrong creating the character: ${data.message}`);
+            return null;
+        }
+
+        return data.character;
+    } catch (error) {
+        console.error(`Error creating a character: ${error}`);
+        return null;
+    }
+};
