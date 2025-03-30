6dfa612bb5c3ba1b45482e602a1f1033b5d84555
amobasse <amobasse@gmail.com>
2025-03-30 00:55:44 -0400

    adding css, adding card components and card data

---
+++ src/components/characterCard.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import { useAuth } from "../App";
+import CharacterCard from '../types/CharacterCard';
+
+const Card = () => {
+    
+    return (
+        // have image with border-radius being 50%
+        // pull character data based on the user
+        // there needs to be a data store for the characters
+        // there has to be a type for the card
+        // somewhere the card has to get the info, either in the card itself or passed in
+        // it has to populate each card based on the user, likely a for loop or foreach
+        // passed in via context...
+        <div className="card">
+            
+        </div>
+    );
+}
+
+export default Card;
---
+++ src/types/CharacterCard.ts
@@ -0,0 +1,12 @@
+type CharacterCard = {
+    id: string,
+    characterName: string,
+    class: string,
+    level: string,
+    race: string,
+    avatar: string,
+    charFC: string,
+    username: string
+}
+
+export default CharacterCard;
