3f2db628943ad1b6183e3dc0908cca1d5976dac3
amobasse <amobasse@gmail.com>
2025-03-30 22:39:30 -0400

    updated dashboard to handle delete

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,9 +1,13 @@
 import { useState, useEffect } from "react";
 import { useAuth } from "../App.tsx";
 import { User } from "../types/User";
-import Card from './Card.tsx';
-import CharacterCard from '../types/CharacterCard.ts';
-import { getUserCharacters, createCharacter } from '../services/characterService';
+import Card from "./Card.tsx";
+import CharacterCard from "../types/CharacterCard.ts";
+import {
+  getUserCharacters,
+  createCharacter,
+  deleteCharacter,
+} from "../services/characterService";
 
 const Dashboard = () => {
   const { user, setUser, showLogin, setShowLogin } = useAuth();
@@ -13,52 +17,68 @@ const Dashboard = () => {
   const handleLogout = () => {
     setUser(null);
     setShowLogin(true);
-  }
+  };
 
   useEffect(() => {
     const fetchCharacters = async () => {
-        if (user?.id) {
-            setLoading(true);
-            const userCharacters = await getUserCharacters(user.id);
-            setCharacters(userCharacters);
-            setLoading(false);
-        }
+      if (user?.id) {
+        setLoading(true);
+        const userCharacters = await getUserCharacters(user.id);
+        setCharacters(userCharacters);
+        setLoading(false);
+      }
     };
 
     fetchCharacters();
   }, [user?.id]);
 
+  const handleDeleteCharacter = async (characterId: string) => {
+    const success = await deleteCharacter(characterId);
+    if (success) {
+      setCharacters((prevChar) =>
+        prevChar.filter((char) => char.id !== characterId)
+      );
+    }
+  };
+
   return (
     <div className="dashboard">
-        <header className="dashboard-header">
-            <h1>Welcome to Chronica, {user?.username}.</h1>
-            <button
-                onClick={handleLogout}
-                className="logout-button"
-            >Logout</button>
-        </header>
-            
-        <div className="user-info">
-            <h2>Your Profile</h2>
-            <p><strong>Username:</strong> {user?.username}</p>
-            <p><strong>Email:</strong> {user?.email}</p>
-        </div>
+      <header className="dashboard-header">
+        <h1>Welcome to Chronica, {user?.username}.</h1>
+        <button onClick={handleLogout} className="logout-button">
+          Logout
+        </button>
+      </header>
 
-        <div className="dashboard-content">
-            <h2>Your Characters</h2>
-            
-            {loading ? (
-                <p>Loading your characters...</p>
-            ) : characters.length > 0 ? (
-                <div className="character-cards">
-                    {characters.map(character => (
-                        <Card key={character.id} character={character} />
-                        ))}
-                </div>
-            ) : (
-                <p>You don't have any characters yet. Create one to get started.</p>
-            )}
-        </div>
+      <div className="user-info">
+        <h2>Your Profile</h2>
+        <p>
+          <strong>Username:</strong> {user?.username}
+        </p>
+        <p>
+          <strong>Email:</strong> {user?.email}
+        </p>
+      </div>
+
+      <div className="dashboard-content">
+        <h2>Your Characters</h2>
+
+        {loading ? (
+          <p>Loading your characters...</p>
+        ) : characters.length > 0 ? (
+          <div className="character-cards">
+            {characters.map((character) => (
+              <Card
+                key={character.id}
+                character={character}
+                onDelete={handleDeleteCharacter}
+              />
+            ))}
+          </div>
+        ) : (
+          <p>You don't have any characters yet. Create one to get started.</p>
+        )}
+      </div>
     </div>
   );
 };
